c7397d51ed533b8520771310f62e74d3f13d247c
marnaroxwd <maxime.baldan11@gmail.com>
2026-01-21 21:41:04 +0100

    Commit 2 - Mise en place du burger menu

---
+++ assets/script/app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.nav__list--menu');
+
+burger.addEventListener('click', () => {
+    const isOpen = menu.classList.toggle('is-open');
+    burger.setAttribute('aria-expanded', isOpen);
+    console.log("test");
+    
+});
